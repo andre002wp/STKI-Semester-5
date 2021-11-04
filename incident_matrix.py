3dcdd52d38e4836e6eef11eb755afe020d3295ef
Hari Anugrah <code.anugrah@gmail.com>
2021-11-05 01:25:43 +0800

    incident matrix use actual index

--- incident_matrix.py
+++ incident_matrix.py
@@ -36,9 +36,60 @@ class BooleanModelIncidentMatrix:  # Incident Matrix
 
             self.incident_matrix[word] = foundIndex
 
+    # Helper function
+    def __keyword_to_binary(self, keyword: 'str') -> 'int':
+        # Urutan dari bit terkiri (Most important) ke kanan (Least important)
+        # Most important adalah dokumen 0
+        # Least important adalah dokumen terakhir
+        a = 0
+        for bit in reversed(self.incident_matrix[keyword]):
+            a = a << 1
+            a = a | bit
+
+        return a
+
     def query(self, q: 'str') -> 'list[Document]':
-        quer = BooleanModelQuerier(self.documents)
-        return quer.query(q)
+        if self.incident_matrix == None:
+            raise Exception("Lakukan indexing sebelum mengquery")
+
+        # region Parsing Query
+        parsed = ApalahParser.parse(q)
+        for i, token in enumerate(parsed.token):
+            if not token.is_symbol:
+                b = self.__keyword_to_binary(token.token)
+                # print(token, bin(b))
+                parsed.token[i].set_binary(b)
+
+        to_eval = ""
+        for it in parsed.token:
+            if it.is_symbol:
+                to_eval += it.token
+            else:
+                to_eval += f" {bin(it.binary)} "
+
+        if to_eval == "":
+            print("Invalid query")
+            return
+        # endregion
+
+        # region Actual Querying
+        res: 'int' = eval(to_eval)  # DANGER, tapi biar cepet yasudahlah
+        print(to_eval, "==>", res)
+
+        result_docs: 'list[Document]' = []
+        # Kenapa reversed?
+        # Karena di `__keyword_to_binary`, index 0 ada di most important bit
+        # sedangkan ketika mereturn, kita mulai dari bit terkanan
+        # Biar konsisten index 0 ada dikiri, kita reverse aja
+        for i, doc in enumerate(reversed(self.documents)):
+            keyword_in_doc: 'int' = res & 1  # Bisa 0, atau bisa 1. Kek bool
+            if keyword_in_doc == 1:
+                result_docs.append(doc)
+            res = res >> 1
+
+        return result_docs
+        # endregion
+
 
 # bukan | (adalah & sama)
 
@@ -46,3 +97,4 @@ class BooleanModelIncidentMatrix:  # Incident Matrix
 # 010 | (001 & 011) = 010 | 001 = 011 = 3
 
 # brutus & caesar & ~calpurnia
+# brutus | caesar & ~calpurnia
